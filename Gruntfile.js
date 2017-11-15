78d4eea449975f3d12a31fc9e896bb72a80a2fd2
Kiril Yurovnik <kyurovnik@gmail.com>
2017-11-15 22:40:24 +0200

    Grunt npm release task

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            dist: ['dist']
+        },
+        copy: {
+            dist: {
+                files: [
+                    { expand: true, cwd: 'src/', src: '**', dest: 'dist/' },
+                    { expand: true, src: 'package.json', dest: 'dist/' }
+                ]
+            }
+        },
+        publish: {
+            dist: {
+                src: ['dist']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-publish');
+
+    grunt.registerTask('release', ['clean', 'copy', 'publish']);
+};
