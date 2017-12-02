452a24c4e548c1edd4dedef21471b510faacf7fa
Kiril Yurovnik <kyurovnik@gmail.com>
2017-12-02 17:47:45 +0200

    Updated example app to use v0.3.3

--- example/routes/CallerAuthenticatingRoute.ts
+++ example/routes/CallerAuthenticatingRoute.ts
@@ -1,32 +1,17 @@
 import { Request, Response } from 'express';
 import * as HttpStatus from 'http-status-codes';
 import Route from 'express-easy-routing/Route';
-import RouteAction from 'express-easy-routing/RouteAction';
 import ActionResult from 'express-easy-routing/results/ActionResult';
 import Controller from 'express-easy-routing/Controller';
 import AuthenticatedCallerController from '../controllers/AuthenticatedCallerController';
 
 abstract class CallerAuthenticatingRoute extends Route {
 
-    protected handleActionRequest(action: RouteAction, request: Request, response: Response): void {
-        const callerId: string = CallerAuthenticatingRoute.getCallerId(request);
-
-        if (callerId !== 'admin') {
-            const unauthorizedResult = Promise.reject(
-                new UnauthorizedCallerError(callerId)
-            );
-
-            super.handleActionResult(unauthorizedResult, response);
-            return;
-        }
-
-        super.handleActionRequest(action, request, response);
-    }
-
     protected initializeController(controller: Controller, request: Request, response: Response): void {
-        super.initializeController(controller, request, response);
         (controller as AuthenticatedCallerController).callerId =
-            CallerAuthenticatingRoute.getCallerId(request);
+            CallerAuthenticatingRoute.authenticateCaller(request);
+
+        super.initializeController(controller, request, response);
     }
 
     protected configureErrorMappings(): void {
@@ -37,8 +22,14 @@ abstract class CallerAuthenticatingRoute extends Route {
 
     protected abstract createController(request: Request): AuthenticatedCallerController;
 
-    private static getCallerId(request: Request): string {
-        return request.header('Caller-Id');
+    private static authenticateCaller(request: Request) {
+        const callerId: string = request.header('Caller-Id');
+
+        if (callerId !== 'admin') {
+            throw new UnauthorizedCallerError(callerId);
+        }
+
+        return callerId;
     }
 }
 
